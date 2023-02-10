154e89ef5545dc66c1619ffc354d663fcf8cd17f
florenciapedreira <florpedreira@gmail.com>
2023-02-10 20:48:59 -0300

    add

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,34 +1,44 @@
 public class Program
     {
-        public static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine("Memorizer Scripture");
-            let frase = ("Trust in the Lord with all your heart and lean not on your own understanding; in all your ways submit to him, and he will make your paths straight. (Proverbs 3:5-6)");
-            Console.log(frase);
+            Console.WriteLine();
+            string frase = ("Trust in the Lord with all your heart and lean not on your own understanding; in all your ways submit to him, and he will make your paths straight. (Proverbs 3:5-6)");
+            Console.WriteLine("Original: {0}", frase);
+            Console.WriteLine();
+            Console.WriteLine("Press enter to start!");
+            Console.ReadLine();
 
-            frase = frase.replace("Trust in the Lord" , "_____ in the ____");
-            Console.log(frase);
+            string replace = frase.Replace("Trust in the Lord" , "_____ in the ____");
+            Console.WriteLine("Replace: {0}", replace);
+            Console.ReadLine();
 
-            frase = frase.replace("Trust in the Lord with all your heart" , "_____ in the ____ with all ____ _____");
-            Console.log(frase);
+            string replace2 = frase.Replace("Trust in the Lord with all your heart and lean not on your own understanding;" , "_____ in the ____ with all ____ _____ and ____ ___ on your own ____________;");
+            Console.WriteLine("Replace: {0}", replace2);
+            Console.ReadLine();
 
-            frase = frase.replace("Trust in the Lord with all your heart and lean not on your own understanding;" , "_____ in the ____ with all ____ _____ and ____ ___ on your own ____________;");
-            Console.log(frase);
+            string replace3 = frase.Replace("Trust in the Lord with all your heart and lean not on your own understanding; in all your ways submit to him," , "_____ in the ____ with all ____ _____ and ____ ___ on your own ____________; in all your ____ submit to ___,");
+            Console.WriteLine("Replace: {0}", replace3);
+            Console.ReadLine();
 
-            frase = frase.replace("Trust in the Lord with all your heart and lean not on your own understanding; in all your ways submit to him," , "_____ in the ____ with all ____ _____ and ____ ___ on your own ____________; in all your ____ submit to ___,");
-            Console.log(frase);
+            string replace4 = frase.Replace("Trust in the Lord with all your heart and lean not on your own understanding; in all your ways submit to him, and he will make your paths straight. (Proverbs 3:5-6)" , "_____ in the ____ with all ____ _____ and ____ ___ on your own ____________; in all your ____ submit to ___, and he ____ ____ your paths ________. (Proverbs 3:5-6)");
+            Console.WriteLine("Replace: {0}", replace4);
+            Console.ReadLine();
 
-            frase = frase.replace("Trust in the Lord with all your heart and lean not on your own understanding; in all your ways submit to him, and he will make your paths straight. (Proverbs 3:5-6)" , "_____ in the ____ with all ____ _____ and ____ ___ on your own ____________; in all your ____ submit to ___, and he ____ ____ your paths ________. (Proverbs 3:5-6)");
-            Console.log(frase);
+            string replace5 = frase.Replace("Trust in the Lord with all your heart and lean not on your own understanding; in all your ways submit to him, and he will make your paths straight. (Proverbs 3:5-6)" , "_____ in ___ ____ with all ____ _____ and ____ ___ on ____ own ____________; in ___ your ____ ______ to ___, and he ____ ____ your paths ________. (Proverbs 3:5-6)");
+            Console.WriteLine("Replace: {0}", replace5);
+            Console.ReadLine();
 
-            frase = frase.replace("Trust in the Lord with all your heart and lean not on your own understanding; in all your ways submit to him, and he will make your paths straight. (Proverbs 3:5-6)" , "_____ in ___ ____ with all ____ _____ and ____ ___ on ____ own ____________; in ___ your ____ ______ to ___, and he ____ ____ your paths ________. (Proverbs 3:5-6)");
-            Console.log(frase);
+            string replace6 = frase.Replace("Trust in the Lord with all your heart and lean not on your own understanding; in all your ways submit to him, and he will make your paths straight. (Proverbs 3:5-6)" , "_____ in ___ ____ ____ all ____ _____ and ____ ___ on ____ ___ ____________; in ___ your ____ ______ to ___, and __ ____ ____ your _____ ________. (Proverbs 3:5-6)");
+            Console.WriteLine("Replace: {0}", replace6);
+            Console.ReadLine();
 
-            frase = frase.replace("Trust in the Lord with all your heart and lean not on your own understanding; in all your ways submit to him, and he will make your paths straight. (Proverbs 3:5-6)" , "_____ in ___ ____ ____ all ____ _____ and ____ ___ on ____ ___ ____________; in ___ your ____ ______ to ___, and __ ____ ____ your _____ ________. (Proverbs 3:5-6)");
-            Console.log(frase);
+            string replace7 = frase.Replace("Trust in the Lord with all your heart and lean not on your own understanding; in all your ways submit to him, and he will make your paths straight. (Proverbs 3:5-6)" , "_____ __ ___ ____ ____ ___ ____ _____ ___ ____ ___ __ ____ ___ ____________; __ ___ ____ ____ ______ __ ___, ___ __ ____ ____ ____ _____ ________. (Proverbs 3:5-6)");
+            Console.WriteLine("Replace: {0}", replace7);
+            Console.ReadLine();
+            
 
-            frase = frase.replace("Trust in the Lord with all your heart and lean not on your own understanding; in all your ways submit to him, and he will make your paths straight. (Proverbs 3:5-6)" , "_____ __ ___ ____ ____ ___ ____ _____ ___ ____ ___ __ ____ ___ ____________; __ ___ ____ ____ ______ __ ___, ___ __ ____ ____ ____ _____ ________. (Proverbs 3:5-6)");
-            Console.log(frase);
             
         }
     }
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -5,5 +5,32 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop04 World!");
+        frase = frase.Replace("Trust in the Lord with all your heart" , "_____ in the ____ with all ____ _____");
+            Console.WriteLine(frase);
+            Console.ReadLine();
+
+            frase = frase.Replace("Trust in the Lord with all your heart and lean not on your own understanding;" , "_____ in the ____ with all ____ _____ and ____ ___ on your own ____________;");
+            Console.WriteLine(frase);
+            Console.ReadLine();
+
+            frase = frase.Replace("Trust in the Lord with all your heart and lean not on your own understanding; in all your ways submit to him," , "_____ in the ____ with all ____ _____ and ____ ___ on your own ____________; in all your ____ submit to ___,");
+            Console.WriteLine(frase);
+            Console.ReadLine();
+
+            frase = frase.Replace("Trust in the Lord with all your heart and lean not on your own understanding; in all your ways submit to him, and he will make your paths straight. (Proverbs 3:5-6)" , "_____ in the ____ with all ____ _____ and ____ ___ on your own ____________; in all your ____ submit to ___, and he ____ ____ your paths ________. (Proverbs 3:5-6)");
+            Console.WriteLine(frase);
+            Console.ReadLine();
+
+            frase = frase.Replace("Trust in the Lord with all your heart and lean not on your own understanding; in all your ways submit to him, and he will make your paths straight. (Proverbs 3:5-6)" , "_____ in ___ ____ with all ____ _____ and ____ ___ on ____ own ____________; in ___ your ____ ______ to ___, and he ____ ____ your paths ________. (Proverbs 3:5-6)");
+            Console.WriteLine(frase);
+            Console.ReadLine();
+
+            frase = frase.Replace("Trust in the Lord with all your heart and lean not on your own understanding; in all your ways submit to him, and he will make your paths straight. (Proverbs 3:5-6)" , "_____ in ___ ____ ____ all ____ _____ and ____ ___ on ____ ___ ____________; in ___ your ____ ______ to ___, and __ ____ ____ your _____ ________. (Proverbs 3:5-6)");
+            Console.WriteLine(frase);
+            Console.ReadLine();
+
+            frase = frase.Replace("Trust in the Lord with all your heart and lean not on your own understanding; in all your ways submit to him, and he will make your paths straight. (Proverbs 3:5-6)" , "_____ __ ___ ____ ____ ___ ____ _____ ___ ____ ___ __ ____ ___ ____________; __ ___ ____ ____ ______ __ ___, ___ __ ____ ____ ____ _____ ________. (Proverbs 3:5-6)");
+            Console.WriteLine(frase);
+            Console.ReadLine();
     }
 }
